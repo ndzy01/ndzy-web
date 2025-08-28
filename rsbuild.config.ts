55b5cd74c2e43f8c0b3be186f9c51ce0241e7f12
ndzywork998 <zhang.yi@pitayagames.com>
2025-08-28 10:27:54 +0800

    优化

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
+const defaultMeta = {
+  charset: {
+    charset: 'UTF-8',
+  },
+  viewport: 'width=device-width,initial-scale=1.0,user-scalable=no',
+};
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: '/music/',
+  },
+  html: {
+    title: 'web',
+    favicon: 'https://cdn.jsdelivr.net/gh/ndzy01/img/ndzy.png',
+    meta: defaultMeta,
+  },
 });
