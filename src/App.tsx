9c0de213dbec84ba9defa858d4dcd715199a4690
ndzy <2326962137@qq.com>
2025-09-09 06:30:52 +0800

    优化

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import './App.css';
 import LiveChat from './components/LiveChat';
 import WorkerDemo from './components/worker-demo';
 import LiveChat1 from './components/LiveChat1';
+import Dialog from 'rc-dialog';
+import 'rc-dialog/assets/index.css';
 
 interface State {
   showLiveChat: boolean;
   showLiveChat1: boolean;
   showWorkerDemo: boolean;
+  openDialog: boolean;
 }
 const App = () => {
   const [state, setState] = useSetState<State>({
     showLiveChat: false,
     showLiveChat1: false,
     showWorkerDemo: false,
+    openDialog: false,
   });
   const handleClick = (key: keyof State) => {
     setState({ [key]: !state[key] } as Pick<State, keyof State>);
@@ -45,6 +49,16 @@ const App = () => {
       {state.showLiveChat && <LiveChat />}
       {state.showLiveChat1 && <LiveChat1 />}
       {state.showWorkerDemo && <WorkerDemo />}
+      <button onClick={() => setState({ openDialog: true })}>打开对话框</button>
+      {state.openDialog && (
+        <Dialog
+          title={'111'}
+          onClose={() => setState({ openDialog: false })}
+          visible={state.openDialog}
+        >
+          <p>first dialog</p>
+        </Dialog>
+      )}
     </>
   );
 };
