093737a0c1cb9b13c2462795b853cbf3ec23b8b9
ndzywork998 <zhang.yi@pitayagames.com>
2025-09-11 15:47:14 +0800

    优化

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('https://www.ndzy01.com/ndzy-web/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker 注册成功: ', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker 注册失败: ', error);
+      });
+  });
+}
+
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
